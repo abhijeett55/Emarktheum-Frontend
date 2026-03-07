74cabd9d0fbb49a8fe5bc99499df81825090b75a
abhijeett55 <100180572+abhijeett55@users.noreply.github.com>
2026-03-07 16:46:57 +0530

    updates

---
+++ src/app/environment/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase : {
+    apiKey: "AIzaSyD6Za6JquL4d0kqMUkhgSph4fG9vImZoII",
+    authDomain: "emarktheum.firebaseapp.com",
+    projectId: "emarktheum",
+    storageBucket: "emarktheum.firebasestorage.app",
+    messagingSenderId: "992968688296",
+    appId: "1:992968688296:web:e4736842839b53833c789d",
+    measurementId: "G-9726RW26PS"
+  }
+  
+};
